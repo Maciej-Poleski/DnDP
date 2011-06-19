0c52b92fd4c49c89d9c56b0aa83c7d018bcf631c
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2011-06-19 20:34:42 +0200

    Widok atutów

--- src/main/java/org/dndp/dndc/client/gui/card/fleats/FleatsView.java
+++ src/main/java/org/dndp/dndc/client/gui/card/fleats/FleatsView.java
@@ -2,6 +2,10 @@ package org.dndp.dndc.client.gui.card.fleats;
 
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.jface.viewers.ListViewer;
 
 public class FleatsView extends Group
 {
@@ -15,6 +19,10 @@ public class FleatsView extends Group
     {
         super(parent, style);
         setText("Atuty");
+        setLayout(new FillLayout(SWT.HORIZONTAL));
+        
+        ListViewer listViewer = new ListViewer(this, SWT.BORDER | SWT.V_SCROLL);
+        List fleetListView = listViewer.getList();
 
     }
 
@@ -23,5 +31,4 @@ public class FleatsView extends Group
     {
         // Disable the check that prevents subclassing of SWT components
     }
-
 }
