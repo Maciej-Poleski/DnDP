daf5b05b86c56c3e0c00f842d6bf2a9bd492d016
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2011-06-20 16:10:47 +0200

    Zrobienie bindingsów do HpView.

--- src/main/java/org/dndp/dndc/client/gui/card/hp/HpView.java
+++ src/main/java/org/dndp/dndc/client/gui/card/hp/HpView.java
@@ -1,24 +1,37 @@
 package org.dndp.dndc.client.gui.card.hp;
 
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import org.dndp.dndc.engine.card.hp.HitPoints;
+import org.eclipse.jface.viewers.deferred.SetModel;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.wb.swt.SWTResourceManager;
 
-public class HpView extends Group
+public class HpView extends Group implements Observer, ModifyListener
 {
     private Text hpText;
     private Text maxHpText;
+    private HitPoints model;
 
     /**
      * Create the composite.
      * @param parent
      * @param style
+     * @param model TODO
      */
-    public HpView(Composite parent, int style)
+    public HpView(Composite parent, int style, HitPoints model)
     {
         super(parent, style);
         setText("PŻ");
@@ -38,6 +51,15 @@ public class HpView extends Group
         maxHpText = new Text(this, SWT.BORDER);
         maxHpText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
+        setModel(model);
+    }
+
+    /**
+     * @param model the model to set
+     */
+    public void setModel(HitPoints model)
+    {
+        this.model = model;
     }
 
     @Override
@@ -46,4 +68,38 @@ public class HpView extends Group
         // Disable the check that prevents subclassing of SWT components
     }
 
+    @Override
+    public void update(Observable o, Object arg)
+    {
+        if(o instanceof HitPoints)
+        {
+            HitPoints tmp=(HitPoints)o;
+            hpText.setText(Integer.toString(tmp.getHP()));
+            maxHpText.setText(Integer.toString(tmp.getMaxHP()));
+        }
+    }
+    
+    @Override
+    public void modifyText(ModifyEvent e)
+    {
+        try
+        {
+            model.setHP(Integer.parseInt(hpText.getText()));
+            hpText.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        }
+        catch(NumberFormatException e1)
+        {
+            hpText.setBackground(SWTResourceManager.getColor(SWT.COLOR_RED));
+        }
+        
+        try
+        {
+            model.setMaxHP(Integer.parseInt(maxHpText.getText()));
+            maxHpText.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        }
+        catch(NumberFormatException e1)
+        {
+            maxHpText.setBackground(SWTResourceManager.getColor(SWT.COLOR_RED));
+        }
+    }
 }
