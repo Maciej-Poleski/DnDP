fada4c51fc54b81c884ee07db493d64768d1f53d
bambucha <bambucha14@gmail.com>
2011-10-28 21:30:17 +0200

    Naprawienie testów dotyczących wartości przedmiotów.
    Fix #86
    Time:0h15m

--- engine/src/test/java/org/dndp/dndc/engine/item/ValueTest.java
+++ engine/src/test/java/org/dndp/dndc/engine/item/ValueTest.java
@@ -1,7 +1,9 @@
 package org.dndp.dndc.engine.item;
 
 import static org.junit.Assert.*;
+import static org.fest.assertions.Assertions.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -11,128 +13,102 @@ import org.junit.Test;
 public class ValueTest
 {
 
-    Value x;
+	Value	x;
 
-    public ValueTest()
-    {
-        x = new Value(4, 132, 22, 33);
-    }
+	public ValueTest()
+	{
+		x = new Value(4, 132, 22, 33);
+	}
 
-    @Test
-    public void testAdd()
-    {
-        System.out.println("add");
-        Value b = new Value(3, 90, 90, 90);
-        Value instance = new Value(4, 132, 22, 33);
-        Value expResult = new Value(30, 4, 4, 3);
-        Value result = Value.add(instance, b);
-        assertEquals(expResult.toString(), result.toString());
-    }
+	@Before
+	public void before()
+	{
+		x = new Value(4, 132, 22, 33);
+	}
 
-    @Test
-    public void testGetCopper()
-    {
-        System.out.println("getCopper");
-        Value instance = x;
-        Integer expResult = 3;
-        Integer result = instance.getCopper();
-        assertEquals(expResult, result);
-    }
+	@Test
+	public void testAdd()
+	{
+		System.out.println("add");
+		Value b = new Value(3, 90, 90, 90);
+		Value instance = new Value(4, 132, 22, 33);
+		Value expResult = new Value(30, 4, 4, 3);
+		assertThat(Value.add(instance, b)).isEqualTo(expResult);
+	}
 
-    @Test
-    public void testSetCopper()
-    {
-        System.out.println("setCopper");
-        Integer copper = 10;
-        Value instance = x;
-        instance.setCopper(copper);
-        assertEquals(0, x.getCopper().intValue());
-    }
+	@Test
+	public void testGetCopper()
+	{
+		assertThat(x.getCopper()).isEqualTo(3);
+	}
 
-    @Test
-    public void testGetGold()
-    {
-        System.out.println("getGold");
-        Value instance = x;
-        Integer expResult = 4;
-        Integer result = instance.getGold();
-        assertEquals(expResult, result);
-    }
+	@Test
+	public void testSetCopper()
+	{
+		x.setCopper(10);
+		assertThat(x.getCopper().intValue()).isEqualTo(0);
+	}
 
-    @Test
-    public void testSetGold()
-    {
-        System.out.println("setGold");
-        Integer gold = 21;
-        Value instance = new Value(4, 132, 22, 33);
-        instance.setGold(gold);
-        assertEquals(1, instance.getGold().intValue());
-    }
+	@Test
+	public void testGetGold()
+	{
+		assertThat(x.getGold()).isEqualTo(4);
+	}
 
-    @Test
-    public void testGetPlatinum()
-    {
-        System.out.println("getPlatinum");
-        Value instance = x;
-        Integer expResult = 17;
-        Integer result = instance.getPlatinum();
-        assertEquals(expResult, result);
-    }
+	@Test
+	public void testSetGold()
+	{
+		x.setGold(21);
+		assertThat(x.getGold()).isEqualTo(1);
+	}
 
-    @Test
-    public void testSetPlatinum()
-    {
-        System.out.println("setPlatinum");
-        Integer platinum = 5;
-        Value instance = x;
-        instance.setPlatinum(platinum);
-        assertEquals(instance.getPlatinum(), platinum);
-    }
+	@Test
+	public void testGetPlatinum()
+	{
+		assertThat(x.getPlatinum()).isEqualTo(17);
+	}
 
-    @Test
-    public void testGetSilver()
-    {
-        System.out.println("getSilver");
-        Value instance = x;
-        Integer expResult = 5;
-        Integer result = instance.getSilver();
-        assertEquals(expResult, result);
-    }
+	@Test
+	public void testSetPlatinum()
+	{
+		x.setPlatinum(5);
+		assertThat(x.getPlatinum()).isEqualTo(5);
+	}
 
-    @Test
-    public void testSetSilver()
-    {
-        System.out.println("setSilver");
-        Integer silver = 30;
-        Value instance = x;
-        instance.setSilver(silver);
-        assertEquals(0, instance.getSilver().intValue());
-    }
+	@Test
+	public void testGetSilver()
+	{
+		assertThat(x.getSilver()).isEqualTo(5);
+	}
 
-    @Test
-    public void testToString()
-    {
-        System.out.println("toString");
-        Value instance = x;
-        String expResult = "17 pp 4 gp 5 sp 3 cp";
-        String result = instance.toString();
-        assertEquals(expResult, result);
-    }
-    
-    @Test
-    public void compareToTest()
-    {
-        Value a = new Value(0, 0, 1, 5);
-        assertTrue(a.compareTo(a) == 0); //Zwrotność
-        Value b = new Value(0, 0, 1, 6);
-        assertTrue(a.compareTo(b) < 0);
-        assertTrue(b.compareTo(a) > 0);
-        a = Value.add(a, new Value(0, 0, 0, 1));
-        assertTrue(a.compareTo(b) == 0);
-        assertEquals(a.compareTo(b) == 0, a.equals(b));
-        a = Value.add(a, new Value(0, 0, 0, 1));
-        assertTrue(a.compareTo(b) > 0);
-        assertTrue(b.compareTo(a) < 0);
-    }
+	@Test
+	public void testSetSilver()
+	{
+		x.setSilver(30);
+		assertThat(x.getSilver()).isEqualTo(0);
+	}
+
+	@Test
+	public void testToString()
+	{
+		System.out.println("toString");
+		String expResult = "17 pp 4 gp 5 sp 3 cp";
+		assertThat(x.toString()).isEqualTo(expResult);
+	}
+
+	@Test
+	public void compareToTest()
+	{
+		Value a = new Value(0, 0, 1, 5);
+		assertThat(a.compareTo(a)).isEqualTo(0);
+		Value b = new Value(0, 0, 1, 6);
+		assertThat(a.compareTo(b)).isNegative();
+		assertThat(b.compareTo(a)).isPositive();
+		a = Value.add(a, new Value(0, 0, 0, 1));
+		assertThat(a.compareTo(b)).isZero();
+		a = Value.add(a, new Value(0, 0, 0, 1));
+		assertThat(a.compareTo(b)).isPositive();
+		assertThat(b.compareTo(a)).isNegative();
+	}
 
 }
