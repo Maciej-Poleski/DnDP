5bc34a6895d0434547bc6ed33bbe637edb5e5096
bambucha <bambucha14@gmail.com>
2011-10-28 20:14:55 +0200

    Poczatek naprawiania problemów re #86 Time:0h15m

--- engine/src/test/java/org/dndp/dndc/engine/item/InventoryTest.java
+++ engine/src/test/java/org/dndp/dndc/engine/item/InventoryTest.java
@@ -1,112 +1,113 @@
 package org.dndp.dndc.engine.item;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.dndp.dndc.engine.benefit.Benefit;
 import org.dndp.dndc.engine.item.things.Belt;
 import org.dndp.dndc.engine.item.things.Coat;
-import org.dndp.dndc.engine.item.things.Helmet;
-import org.dndp.dndc.engine.item.things.Shirt;
 import org.dndp.dndc.engine.item.things.Shoes;
+import org.fest.assertions.Delta;
+import org.junit.Before;
 import org.junit.Test;
-
 
 public class InventoryTest
 {
-    Benefit[] benefits      = new Benefit[0];
-    Item      belt          = new Belt("belt", 0.5, new Value(0, 1, 2, 3), benefits);
-    Item      coat          = new Coat("coat", 1.3, new Value(1, 0, 0, 7), benefits);
-    Item      shoes         = new Shoes("shoes", 0.4, new Value(0, 0, 7, 5), benefits);
+	Item		belt;
+	Item		coat;
+	Item		second;
+	Inventory	testInventory;
+	Inventory	inventory;
 
-    Item      dupa          = new Shirt("sss", 1.0, new Value(1, 1, 1, 1), benefits);
-    Item      dupa2         = new Helmet("ss", 1.0, new Value(1, 1, 1, 1), benefits);
-    Inventory testInventory = new Inventory("sd", 3.0, new Value(5, 5, 5, 5), benefits);
+	@Before
+	public void before()
+	{
+		belt = mock(Belt.class);
+		when(belt.getName()).thenReturn("belt");
+		when(belt.getWeight()).thenReturn(0.5);
+		when(belt.getValue()).thenReturn(new Value(0, 1, 2, 3));
 
-    Inventory inventory     = new Inventory("inv", 1.3, new Value(0, 0, 0, 5), benefits);
+		coat = mock(Coat.class);
+		when(coat.getName()).thenReturn("coat");
+		when(coat.getWeight()).thenReturn(1.3);
+		when(coat.getValue()).thenReturn(new Value(1, 0, 0, 7));
 
-    public InventoryTest()
-    {
-        testInventory.add(dupa);
-        testInventory.add(dupa2);
-    }
+		second = mock(Shoes.class);
+		when(second.getName()).thenReturn("second");
+		when(second.getWeight()).thenReturn(1.0);
+		when(second.getValue()).thenReturn(new Value(1, 1, 1, 0));
 
-    @Test
-    public void testGetInventory()
-    {
-        assertNotNull(inventory.getInventory());
-    }
+		inventory = new Inventory("inv", 1.0, new Value(0, 0, 0, 5),
+				new Benefit[0]);
+		inventory.add(second);
 
-    @Test
-    public void testAdd()
-    {
-        inventory.add(belt);
-        assertEquals(1.8, inventory.getWeight().doubleValue(), 0.1);
-        assertEquals(new Value(0, 1, 2, 8).toString(), inventory.getValue().toString());
-        inventory.add(coat);
-        assertEquals(3.1, inventory.getWeight().doubleValue(), 0.1);
-        assertEquals(new Value(1, 1, 3, 5).toString(), inventory.getValue().toString());
-        inventory.add(shoes);
-        assertEquals(3.5, inventory.getWeight().doubleValue(), 0.1);
-        assertEquals(new Value(1, 2, 1, 0).toString(), inventory.getValue().toString());
-        inventory.add(testInventory);
-        assertEquals(8.5, inventory.getWeight().doubleValue(), 0.1);
-        assertEquals(new Value(8, 9, 8, 7).toString(), inventory.getValue().toString());
-    }
+		testInventory = new Inventory("a", 2.0, new Value(0, 0, 0, 5),
+				new Benefit[0]);
+	}
 
-    @Test
-    public void testRemove()
-    {
-        inventory.add(belt);
-        inventory.add(coat);
-        inventory.add(shoes);
-        inventory.add(testInventory);
-        assertEquals(8.5, inventory.getWeight().doubleValue(), 1);
-        assertEquals(new Value(8, 9, 8, 7).toString(), inventory.getValue().toString());
-        inventory.remove(testInventory);
-        assertEquals(3.5, inventory.getWeight().doubleValue(), 1);
-        assertEquals(new Value(1, 2, 1, 0).toString(), inventory.getValue().toString());
-        inventory.remove(shoes);
-        assertEquals(3.1, inventory.getWeight().doubleValue(), 1);
-        assertEquals(new Value(1, 1, 3, 5).toString(), inventory.getValue().toString());
-        inventory.remove(coat);
-        assertEquals(1.8, inventory.getWeight().doubleValue(), 1);
-        assertEquals(new Value(0, 1, 2, 8).toString(), inventory.getValue().toString());
-        inventory.remove(belt);
-        assertEquals(1.3, inventory.getWeight().doubleValue(), 1);
-        assertEquals(new Value(0, 0, 0, 5).toString(), inventory.getValue().toString());
+	@Test
+	public void testGetInventory()
+	{
+		assertThat(testInventory.getInventory()).isNotNull();
+	}
 
-    }
+	@Test
+	public void testAdd()
+	{
+		testInventory.add(belt);
+		assertThat(testInventory.getWeight().doubleValue()).isEqualTo(2.5,
+				Delta.delta(0.0001));
+		assertThat(testInventory.getValue()).isEqualTo(new Value(0, 1, 2, 8));
+		testInventory.add(coat);
+		assertThat(testInventory.getWeight().doubleValue()).isEqualTo(3.8,
+				Delta.delta(0.0001));
+		assertThat(testInventory.getValue()).isEqualTo(new Value(1, 1, 3, 5));
+	}
 
-    @Test
-    public void testContainsByName()
-    {
-        inventory.add(belt);
-        inventory.add(coat);
-        inventory.add(shoes);
-        inventory.add(testInventory);
+	@Test
+	public void testRemove()
+	{
+		testInventory.add(belt);
+		testInventory.add(coat);
+		testInventory.add(inventory);
+		assertThat(testInventory.getWeight()).isEqualTo(5.8);
+		assertThat(testInventory.getValue()).isEqualTo(new Value(2, 2, 5, 0));
+		testInventory.remove(inventory);
+		assertThat(testInventory.getWeight()).isEqualTo(3.8);
+		assertThat(testInventory.getValue()).isEqualTo(new Value(1, 1, 3, 5));
+		testInventory.remove(coat);
+		assertThat(testInventory.getWeight()).isEqualTo(2.5);
+		assertThat(testInventory.getValue()).isEqualTo(new Value(1, 1, 3, 5));
+		testInventory.remove(belt);
+		assertThat(testInventory.getWeight()).isEqualTo(2.0);
+		assertThat(testInventory.getValue()).isEqualTo(new Value(0, 0, 0, 5));
 
-        assertTrue(inventory.containsByName("ss"));
-        assertTrue(inventory.containsByName("sss"));
+	}
 
-        assertTrue(inventory.containsByName("belt"));
-        assertTrue(inventory.containsByName("coat"));
-    }
+	@Test
+	public void testContainsByName()
+	{
+		testInventory.add(belt);
+		testInventory.add(coat);
+		testInventory.add(inventory);
 
-    @Test
-    public void testContainsByExample()
-    {
-        inventory.add(belt);
-        inventory.add(coat);
-        inventory.add(shoes);
-        inventory.add(testInventory);
+		assertThat(testInventory.containsByName("second")).isTrue();
 
-        assertTrue(inventory.containsByExample(dupa));
-        assertTrue(inventory.containsByExample(dupa2));
+		assertThat(testInventory.containsByName("belt")).isTrue();
+		assertThat(testInventory.containsByName("coat")).isTrue();
+	}
 
-        assertTrue(inventory.containsByExample(belt));
-        assertTrue(inventory.containsByExample(coat));
-    }
+	@Test
+	public void testContainsByExample()
+	{
+		testInventory.add(belt);
+		testInventory.add(coat);
+		testInventory.add(inventory);
+
+		assertThat(testInventory.containsByExample(second)).isTrue();
+		assertThat(testInventory.containsByExample(belt)).isTrue();
+		assertThat(testInventory.containsByExample(coat)).isTrue();
+	}
 
 }
