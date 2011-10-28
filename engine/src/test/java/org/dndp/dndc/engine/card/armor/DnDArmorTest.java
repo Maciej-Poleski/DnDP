45724c580fa95a0e9b8d87d6095b380e99c03827
bambucha <bambucha14@gmail.com>
2011-10-28 22:18:53 +0200

    Naprawienie testów od klasy pancerza.
    Fix #88
    Time:0h30m

--- engine/src/test/java/org/dndp/dndc/engine/card/armor/DnDArmorTest.java
+++ engine/src/test/java/org/dndp/dndc/engine/card/armor/DnDArmorTest.java
@@ -1,19 +1,22 @@
 /*
- * To change this template, choose Tools | Templates and open the template in the editor.
+ * To change this template, choose Tools | Templates and open the template in
+ * the editor.
  */
 
 package org.dndp.dndc.engine.card.armor;
 
-import static org.junit.Assert.assertEquals;
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import org.dndp.dndc.engine.FantasyCharacter;
-import org.dndp.dndc.engine.benefit.Benefit;
-import org.dndp.dndc.engine.card.bonus.DnDBonusManager;
-import org.dndp.dndc.engine.item.Value;
+import org.dndp.dndc.engine.card.abilities.Abiliti;
+import org.dndp.dndc.engine.card.abilities.Abilities;
+import org.dndp.dndc.engine.card.bonus.ArmorBonusHandler;
+import org.dndp.dndc.engine.card.bonus.BonusManager;
+import org.dndp.dndc.engine.item.EquipmentManager;
 import org.dndp.dndc.engine.item.armor.Plate;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
-
 
 /**
  * 
@@ -21,88 +24,64 @@ import org.junit.Test;
  */
 public class DnDArmorTest
 {
-    FantasyCharacter main;
-    DnDBonusManager  bonusManager;
-    DnDArmor         t;
+	BonusManager		bonus;
+	Abilities			abilities;
+	Abiliti				dexterity;
+	EquipmentManager	manager;
+	ArmorBonusHandler	armorBonusHandler;
+	DnDArmor			armor;
+	Plate				plate;
+	Plate				nd;
 
-    Plate            plate;
-    Plate            nd;
+	public DnDArmorTest()
+	{}
 
-    public DnDArmorTest()
-    {
-        main = new FantasyCharacter();
-        bonusManager = new DnDBonusManager(main,main);
-        t = new DnDArmor(main, main, bonusManager);
-        plate = new Plate("xxx", 1.0, new Value(1, 2, 1, 1), new Benefit[0], 1, 1.0, 1);
-        nd = new Plate("xxx", 1.0, new Value(1, 2, 1, 1), new Benefit[0], 1, 1.0, 3);
-    }
+	@Before
+	public void dupa()
+	{
 
-    @BeforeClass
-    public static void dupa()
-    {
+		bonus = mock(BonusManager.class);
+		armorBonusHandler = mock(ArmorBonusHandler.class);
+		when(bonus.getBonusHandler("Armor")).thenReturn(armorBonusHandler);
+		when(armorBonusHandler.getACBonus()).thenReturn(0);
+		manager = mock(EquipmentManager.class);
+		plate = mock(Plate.class);
+		when(manager.getArmor()).thenReturn(plate);
+		abilities = mock(Abilities.class);
+		dexterity = mock(Abiliti.class);
+		when(abilities.getDexterity()).thenReturn(dexterity);
+		armor = new DnDArmor(abilities, manager, bonus);
+	}
 
-    }
+	/**
+	 * Test of getAC method, of class DnDArmor.
+	 */
+	@Test
+	public void testGetAC()
+	{
+		assertThat(armor.getAC()).isEqualTo(10);
+	}
 
-    /**
-     * Test of getAC method, of class DnDArmor.
-     */
-    @Test
-    public void testGetAC()
-    {
-        System.out.println("getAC");
-        DnDArmor instance = t;
-        Integer expResult = 10;
-        Integer result = instance.getAC();
-        assertEquals(expResult, result);
-    }
+	/**
+	 * Test of getAC method, of class DnDArmor.
+	 */
+	@Test
+	public void testGetACDex()
+	{
+		when(dexterity.getModifier()).thenReturn(5);
+		when(manager.getMaximumDexterityACBonus()).thenReturn(10);
+		assertThat(armor.getAC()).isEqualTo(15);
+	}
 
-    /**
-     * Test of getAC method, of class DnDArmor.
-     */
-    @Test
-    public void testGetACDex()
-    {
-        main.getDexterity().setValue(20);
-        System.out.println("getAC");
-        DnDArmor instance = t;
-        Integer expResult = 15;
-        Integer result = instance.getAC();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of getAC method, of class DnDArmor.
-     */
-    @Test
-    public void testGetACPlate()
-    {
-        System.out.println("getAC");
-        main.getDexterity().setValue(20);
-        main.putOn(plate);
-        DnDArmor instance = t;
-        Integer expResult = 11;
-        Integer result = instance.getAC();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of getAC method, of class DnDArmor.
-     */
-    @Test
-    public void testGetACCPlate()
-    {
-        System.out.println("getAC");
-        main.getDexterity().setValue(20);
-        main.putOn(plate);
-        DnDArmor instance = t;
-        Integer expResult = 11;
-        Integer result = instance.getAC();
-        assertEquals(expResult, result);
-        main.putOn(nd);
-        expResult = 13;
-        result = instance.getAC();
-        assertEquals(expResult, result);
-
-    }
+	/**
+	 * Test of getAC method, of class DnDArmor.
+	 */
+	@Test
+	public void testGetACPlate()
+	{
+		abilities.getDexterity().setValue(20);
+		manager.putOn(plate);
+		assertThat(armor.getAC()).isEqualTo(10);
+	}
 
 }
